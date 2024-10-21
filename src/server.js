e647ecdc1a6aa2ae7bd6f4a2290e5b57dc03fdc9
emilykang <strongwomankky@gmail.com>
2024-10-22 02:55:34 +0900

    환경설정 완료

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
-
+//pug페이지를 렌더링하는 부분
+app.set("view engine","pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/",(req, res) => res.render("home"));
+app.get("/*",(req, res) => res.render("home"));
 
 const handleListen = () => console.log("listening on http://localhost:3000");
-app.listen(3000);
+app.listen(3000, handleListen);
